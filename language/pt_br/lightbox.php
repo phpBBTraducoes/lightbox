2f2d5b6abd6af1d244d2326ddb79e4997fb9b05a
eunaumtenhoid <eunaumtenhoid@outlook.com>
2019-01-03 11:27:14 -0200

    Update

--- language/pt_br/lightbox.php
+++ language/pt_br/lightbox.php
@@ -2,7 +2,8 @@
 /**
  *
  * Lightbox extension for the phpBB Forum Software package.
- * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.5] (https://github.com/phpBBTraducoes)
+ * [Brazilian Portuguese]
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 2.0.0] (https://github.com/phpBBTraducoes)
  * @copyright (c) 2015 Matt Friedman
  * @license GNU General Public License, version 2 (GPL-2.0)
  * 
@@ -23,15 +24,19 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'LIGHTBOX_SETTINGS'				=> 'Redimensionamento de imagem Lightbox',
-	'LIGHTBOX_MAX_WIDTH'			=> 'Largura máxima da imagem em pixels',
-	'LIGHTBOX_MAX_WIDTH_EXPLAIN'	=> 'Imagens que excederem este tamanho serão reduzidas e poderão ser vistas em tamanho maior usando o efeito Lightbox. Deixando em 0 desabilita o redimensionamento.',
-	'LIGHTBOX_MAX_WIDTH_APPEND'		=> 'Recomendação com base nas configurações de anexo da imagem: %spx',
-	'LIGHTBOX_GALLERY'				=> 'Permite modo galeria',
+	'LIGHTBOX_MAX_WIDTH'			=> 'Largura máxima da imagem',
+	'LIGHTBOX_MAX_WIDTH_EXPLAIN'	=> 'Imagens que excedam essa largura serão redimensionadas e podem ser ampliadas usando o efeito Lightbox. Defina este valor como 0 para desativar o redimensionamento da imagem do Lightbox por largura.',
+	'LIGHTBOX_MAX_WIDTH_APPEND'		=> 'Recomendação baseada em configurações de anexo de imagem: %spx',
+	'LIGHTBOX_MAX_HEIGHT'			=> 'Altura máxima da imagem',
+	'LIGHTBOX_MAX_HEIGHT_EXPLAIN'	=> 'Imagens que excedam essa altura serão redimensionadas e podem ser ampliadas usando o efeito Lightbox. Defina este valor como 0 para desativar o redimensionamento da imagem do Lightbox por altura.',
+	'LIGHTBOX_ALL_IMAGES'			=> 'Incluir todas as imagens no efeito Lightbox',
+	'LIGHTBOX_ALL_IMAGES_EXPLAIN'	=> 'Com essa configuração ativada, todas as imagens postadas podem ser abertas no efeito Lightbox, mesmo que não estejam sendo redimensionadas.',
+	'LIGHTBOX_GALLERY'				=> 'Modo galeria',
 	'LIGHTBOX_GALLERY_EXPLAIN'		=> 'Permite fácil navegação entre todas as imagens redimensionadas da página usando o efeito Lightbox.',
 	'LIGHTBOX_GALLERY_ALL'			=> 'Todas as imagens redimensionadas na página',
 	'LIGHTBOX_GALLERY_POSTS'		=> 'Todas as imagens redimensionadas por post',
 	'LIGHTBOX_SIGNATURES'			=> 'Redimensiona imagens na assinatura',
 	'LIGHTBOX_SIGNATURES_EXPLAIN'	=> 'Permite que imagens usadas em assinaturas sejam redimensionadas.',
 	'LIGHTBOX_IMG_TITLES'			=> 'Mostra o nome de arquivo da imagem',
-	'LIGHTBOX_IMG_TITLES_EXPLAIN'	=> 'Os nomes das imagens aparecerão como rodapé no efeito Lightbox.',
+	'LIGHTBOX_IMG_TITLES_EXPLAIN'	=> 'Os nomes das imagens aparecerão como legendas no efeito Lightbox.',
 ));
